83a4772a05c276fdc7aee45090e20a4a62244117
Muhannad <muhanned9980@gmail.com>
2024-10-26 10:42:19 +0300

    adding the form and checking if the data comes into the database

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,14 +4,27 @@ const router = express.Router();
 const User = require("../models/user.js");
 const Recipe = require("../models/recipe.js");
 // routes will be under here
-
+//rendering the landing page
 router.get("/", async (req, res) => {
   res.render("recipes/index.ejs");
 });
-
+//rendering the new page 
 router.get("/new", async (req, res) => {
 
   res.render("recipes/new.ejs")
 });
 
+
+router.post('/' , async ( req , res) =>{
+try{
+req.body.owner = req.session.user._id;
+await Recipe.create(req.body);
+
+res.redirect('/recipes')
+} catch(error){
+  console.log(error)
+  res.redirect('recipes')
+}
+
+})
 module.exports = router;
